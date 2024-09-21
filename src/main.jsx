f40bf6387087a6b0116cca57123bd9c355a0ea2a
Harsh raj <harshraj17087@gmail.com>
2024-09-22 00:40:18 +0530

    changed config

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import singleSpaReact from 'single-spa-react';
+
+// Single-SPA react adapter
+const reactLifecycles = singleSpaReact({
+  React,
+  ReactDOM,
+  rootComponent: App,
+  domElementGetter: () => document.getElementById('root'),
+  errorBoundary(err, info, props) {
+    console.error(err, info, props);
+    return <div>Something went wrong</div>;
+  }
+});
+export const bootstrap = reactLifecycles.bootstrap
+export const mount = reactLifecycles.mount
+export const unmount = reactLifecycles.unmount
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
--- vite.config.js
+++ vite.config.js
@@ -19,6 +19,7 @@ export default defineConfig({
         globals: {
           react: 'React',
           'react-dom': 'ReactDOM',
+          'single-spa': 'singleSpa',
         },
       },
     },
